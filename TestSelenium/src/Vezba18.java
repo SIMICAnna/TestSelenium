b0453ace02ba393c83dbb7b1a4960ae5db1fa12e
SIMICAnna <miljojkovicana90@gmail.com>
2023-10-12 12:24:40 +0200

    premesteno

---
+++ TestSelenium/src/Vezba18.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Vezba18 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("unesi broj");
+
+    }
+
+
+
+    }
+
